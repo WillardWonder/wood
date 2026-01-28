d7a99ea6c1906c27b74511ec14cc4f46475badef
WillardWonder <126423311+WillardWonder@users.noreply.github.com>
2026-01-28 08:22:22 -0600

    Remove rollupOptions from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths work on GitHub Pages
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        market: resolve(__dirname, 'market.html'), // <--- The new entry point
-      },
-    },
-  },
+  base: './', // Crucial for GitHub Pages hosting
 })
